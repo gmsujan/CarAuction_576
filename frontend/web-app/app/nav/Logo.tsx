01deb0523e7e61799e515b83f2e737da12c427d3
gmsujan <sujanmagar6861@gmail.com>
2025-05-05 22:10:25 +1200

    Authorization Done

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,15 +1,24 @@
 'use client'
 
-import { useParamsStore } from '@/hooks/useParamsStore'
+import { useParamsStore } from '@/hooks/useParamsStore';
+import { usePathname} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import React from 'react'
-import { FaCar } from 'react-icons/fa'
+import { FaCar } from 'react-icons/fa';
 
 export default function Logo() {
-    const reset = useParamsStore(state => state.reset);
+    const router = useRouter();
+    const pathname = usePathname();
 
+    function doReset() {
+        if (pathname !== '/') router.push('/');
+        reset();
+    }
+    
+    const reset = useParamsStore(state => state.reset)
 
     return (
-        <div onClick={reset} className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
+        <div onClick={doReset} className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
             <FaCar size={34}/>
             <div>Auctions</div>
         </div>
